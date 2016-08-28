5adccc2244dc088dc4a5fd234fe31e24ee3dbea3
luckystarzwj <1062219692@qq.com>
2016-08-28 17:44:57 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,49 @@
 
+    function move(obj,json,fn){ clearInterval(obj.timer) obj.timer = setInterval(function(){
+    
+            var btn = true 
+            var old = 0 
+    
+            for(var attr in json){
+    
+            if(attr=='opacity'){
+                old=parseFloat(getStyle(obj,'opacity'))*100             
+            }else{
+                old=parseInt(getStyle(obj,attr))
+            }
+    
+            var speed = ( json[attr] - old ) / 8
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed)
+            old += speed
+    
+            if(json[attr] != old){
+                 btn=false
+            }
+    
+            if(attr == 'opacity'){
+                obj.style.opacity = old/100
+            }else{
+                obj.style[attr] = old+'px'
+            }
+    
+            if(btn){
+                clearInterval(obj.timer)
+                if(fn){
+                    fn.call(obj)
+                }
+            }
+        }
+    
+      },30)
+    
+    }
+    
+    function getStyle(obj,attr){
+         if(obj.currentStyle){
+    
+            return obj.currentStyle[attr]
+         }else{
+            return getComputedStyle(obj,false)[attr]
+         }
+    }
+
